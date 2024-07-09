79be0c5a35fee3a0cb142e885c3cfb90a91dee63
Ronaldo Vecchi <rvecchi@gmail.com>
2024-07-09 05:49:00 -0600

    updates

--- modules/servicebus-subscription-rule/main.tf
+++ modules/servicebus-subscription-rule/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_servicebus_subscription_rule" "servucebus_subscription_rule" {
   subscription_id     = var.subscription_id
-  name                = "Label-${var.correlation_filter_label}"
+  name                = "label-${var.correlation_filter_label}"
   filter_type         = "CorrelationFilter"
 
   correlation_filter {
