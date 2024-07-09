a79fa699d6b4a18a76637e79a88c3471cf5be455
Ronaldo Vecchi <rvecchi@gmail.com>
2024-07-08 20:57:08 -0600

    updates

--- modules/servicebus-subscription-rule/variables.tf
+++ modules/servicebus-subscription-rule/variables.tf
@@ -1,36 +1,3 @@
-variable "sub" {
-  type    = string
-  default = "vec"
-}
-
-variable "region" {
-  type	= string
-  default = "eus"
-}
-
-variable "environment" {
-  type    = string
-  default = "dev"
-}
-
-variable "domain" {
-  type = string
-}
-
-variable "sequence" {
-  type    = string
-  default = "001"
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type    = string
-  default = "East US"
-}
-
 variable "subscription_id" {
   type    = string
 }
