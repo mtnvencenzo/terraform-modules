2b1d169ebcf353318f6464cc06a01f5f9164260b
Ronaldo Vecchi <rvecchi@gmail.com>
2024-07-19 05:59:55 -0600

    updates

--- modules/servicebus-namespace/main.tf
+++ modules/servicebus-namespace/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_servicebus_namespace" "servicebus_namespace" {
     # }
     
     lifecycle {
-        prevent_destroy = false
+        prevent_destroy = true
     }
 
     tags = {
