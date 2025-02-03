3c4c1ac220a23e4e8d57fc47d209f670032f340f
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-02 21:07:08 -0700

    updates

--- modules/apim-api/apim_api_product_subscriptions.tf
+++ modules/apim-api/apim_api_product_subscriptions.tf
@@ -6,7 +6,7 @@ resource "azurerm_api_management_subscription" "apim_api_product_subscription" {
   product_id            = azurerm_api_management_product.apim_api_product.id
   display_name          = each.value.name
   state                 = "active"
-  subscription_id       = "${replace(" ", lower(each.name), "-")}-subscription"
+  subscription_id       = "${replace(" ", lower(each.value.name), "-")}-subscription"
   primary_key           = each.value.primary_key
   secondary_key         = each.value.secondary_key
   allow_tracing         = false
