424e309f74cb6fd9dd4e4791b5808fd9c0d620cd
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-02 18:43:02 -0700

    updates

--- modules/container-app/aca_access_keyvault.tf
+++ modules/container-app/aca_access_keyvault.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault_access_policy" "aca_keyvault_policy" {
     count                               = var.key_vault != null ? 1 : 0
     key_vault_id                        = var.key_vault.id
     tenant_id                           = var.tenant_id
-    object_id                           = azurerm_user_assigned_identity.aca.principal_id
+    object_id                           = azurerm_user_assigned_identity.aca_user_identity.principal_id
     key_permissions                     = ["Get", "List"]
     secret_permissions                  = ["Get", "List"]
 
