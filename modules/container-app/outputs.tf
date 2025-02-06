51e196e361fa07ef666cc076c33458a45394edcf
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-05 17:40:33 -0700

    Adding managed identiy outputes to aca module

--- modules/container-app/outputs.tf
+++ modules/container-app/outputs.tf
@@ -10,3 +10,11 @@ output "name" {
 output "ingress_fqdn" {
     value = azurerm_container_app.aca.ingress[0].fqdn
 }
+
+output "managed_identity_name" {
+    value = azurerm_user_assigned_identity.aca_user_identity.name
+}
+
+output "managed_identity_id" {
+    value = azurerm_user_assigned_identity.aca_user_identity.id
+}
