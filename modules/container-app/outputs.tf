74a3d4f42834e71ead2ecf96a74660ba86fdb521
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-05 17:42:08 -0700

    updates

--- modules/container-app/outputs.tf
+++ modules/container-app/outputs.tf
@@ -18,3 +18,8 @@ output "managed_identity_name" {
 output "managed_identity_id" {
     value = azurerm_user_assigned_identity.aca_user_identity.id
 }
+
+output "managed_identity_principal_id" {
+    value = azurerm_user_assigned_identity.aca_user_identity.principal_id
+}
+
