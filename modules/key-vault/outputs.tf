fa702cc49380e37e908ccda7f74cf5bb659331c6
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-06 18:55:36 -0700

    removing subnets from keyvault outputs

--- modules/key-vault/outputs.tf
+++ modules/key-vault/outputs.tf
@@ -6,7 +6,3 @@ output "name" {
     value = azurerm_key_vault.keyvault.name
 }
 
-output "virtual_network_subnet_ids" {
-    value = azurerm_key_vault.keyvault.network_acls.virtual_network_subnet_ids
-}
-
