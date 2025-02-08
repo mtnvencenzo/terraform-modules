3e9f1b2d5f1fa4cec270285caf24a0d0f0acd0b5
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-08 15:40:35 -0700

    updates

--- modules/apim-api/apim_api_operations.tf
+++ modules/apim-api/apim_api_operations.tf
@@ -29,22 +29,22 @@ resource "azurerm_api_management_api_operation" "apim_api_version_operations" {
   }
 }
 
-resource "azurerm_api_management_api_operation_policy" "apim_api_version_operations_policy" {
-  for_each = tomap({
-    for operation in var.operations :
-      "v${var.api.version}_${operation.method}_${operation.url_template}" => operation
-  })
+# resource "azurerm_api_management_api_operation_policy" "apim_api_version_operations_policy" {
+#   for_each = tomap({
+#     for operation in var.operations :
+#       "v${var.api.version}_${operation.method}_${operation.url_template}" => operation
+#   })
 
-  api_name              = azurerm_api_management_api.apim_api_version.name
-  api_management_name   = var.apim_instance.name
-  resource_group_name   = var.apim_instance.resource_group_name
-  operation_id          = replace(lower("v${var.api.version}-${each.value.method}-${each.value.display_name}"), " ", "-")
+#   api_name              = azurerm_api_management_api.apim_api_version.name
+#   api_management_name   = var.apim_instance.name
+#   resource_group_name   = var.apim_instance.resource_group_name
+#   operation_id          = replace(lower("v${var.api.version}-${each.value.method}-${each.value.display_name}"), " ", "-")
 
-  xml_content           = each.value.security_type == "anonymous" ? local.anonomous_operation_policy : local.b2c_auth_operation_policy
+#   xml_content           = each.value.security_type == "anonymous" ? local.anonomous_operation_policy : local.b2c_auth_operation_policy
 
-  depends_on = [ 
-    azurerm_api_management_api_operation.apim_api_version_operations,
-    azurerm_api_management_policy_fragment.apim_api_cors_policy_fragment,
-    azurerm_api_management_policy_fragment.apim_api_b2c_policy_fragment
-  ]
-}
+#   depends_on = [ 
+#     azurerm_api_management_api_operation.apim_api_version_operations,
+#     azurerm_api_management_policy_fragment.apim_api_cors_policy_fragment,
+#     azurerm_api_management_policy_fragment.apim_api_b2c_policy_fragment
+#   ]
+# }
