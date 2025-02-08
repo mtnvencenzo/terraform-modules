2ac79876e2c8250fcc86b1777cd2175e23bf8e23
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-08 11:15:04 -0700

    reverting back operation id specs

--- modules/apim-api/apim_api_operations.tf
+++ modules/apim-api/apim_api_operations.tf
@@ -1,14 +1,14 @@
 resource "azurerm_api_management_api_operation" "apim_api_version_operations" {
   for_each = tomap({
     for operation in var.operations :
-      operation.operation_id => operation
+      "v${var.api.version}_${operation.method}_${operation.url_template}" => operation
   })
 
   api_name              = azurerm_api_management_api.apim_api_version.name
   api_management_name   = var.apim_instance.name
   resource_group_name   = var.apim_instance.resource_group_name
 
-  operation_id          = each.value.operation_id
+  operation_id          = replace(lower("v${var.api.version}-${each.value.method}-${each.value.display_name}"), " ", "-")
   method                = "${upper(each.value.method)}"
   display_name          = each.value.display_name
   url_template          = each.value.url_template
@@ -35,7 +35,7 @@ resource "azurerm_api_management_api_operation_policy" "apim_api_version_operati
   api_name              = azurerm_api_management_api.apim_api_version.name
   api_management_name   = var.apim_instance.name
   resource_group_name   = var.apim_instance.resource_group_name
-  operation_id          = var.operations[count.index].operation_id
+  operation_id          = replace(lower("v${var.api.version}-${var.operations[count.index].method}-${var.operations[count.index].display_name}"), " ", "-")
 
   xml_content           = var.operations[count.index].security_type == "anonymous" ? local.anonomous_operation_policy : local.b2c_auth_operation_policy
 
