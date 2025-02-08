8adc3a3162081d84a8da8d621b1c6ac8d2f4a72e
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-08 15:05:53 -0700

    updates

--- modules/apim-api/apim_api_operations.tf
+++ modules/apim-api/apim_api_operations.tf
@@ -35,7 +35,7 @@ resource "azurerm_api_management_api_operation_policy" "apim_api_version_operati
   api_name              = azurerm_api_management_api.apim_api_version.name
   api_management_name   = var.apim_instance.name
   resource_group_name   = var.apim_instance.resource_group_name
-  operation_id          = replace(lower("v${var.api.version}-${var.operations[count.index].method}-${var.operations[count.index].display_name}"), " ", "-")
+  operation_id          = azurerm_api_management_api_operation.apim_api_version_operations[count.index].operation_id
 
   xml_content           = var.operations[count.index].security_type == "anonymous" ? local.anonomous_operation_policy : local.b2c_auth_operation_policy
 
