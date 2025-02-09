4a92e0a6d37ebf35988471b2b1c2bfcc0f6ddd43
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-09 08:58:00 -0700

    Adding mds-mx-record-module

---
+++ modules/dns-mx-record/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_dns_mx_record" "dns_mx_record" {
+  name                = "@"
+  zone_name           = var.dns_zone.name
+  resource_group_name = var.dns_zone.resource_group_name
+  ttl                 = var.ttl
+
+  dynamic "record" {
+    for_each = toset(var.record_exchanges)
+
+    content {
+      preference  = env.value["preference"]
+      exchange    = env.value["exchange"]
+    }
+  }
+}
+
+resource "azurerm_dns_txt_record" "dns_mx_spf_record" {
+  name                = "@"
+  zone_name           = var.dns_zone.name
+  resource_group_name = var.dns_zone.resource_group_name
+  ttl                 = var.ttl
+
+  record {
+    value = "v=spf1 include:${var.spf_include_domain} ~all"
+  }
+}
+
+resource "azurerm_dns_txt_record" "dns_mx_dkim_record" {
+  name                = var.dkim_record.name
+  zone_name           = var.dns_zone.name
+  resource_group_name = var.dns_zone.resource_group_name
+  ttl                 = var.ttl
+
+  record {
+    value = var.dkim_record.value
+  }
+}
---
+++ modules/dns-mx-record/variables.tf
@@ -0,0 +1,29 @@
+variable "dns_zone" {
+  type = object({
+    name                = string
+    resource_group_name = string
+  })
+}
+
+variable "ttl" {
+  type = number
+  default = 300
+}
+
+variable "spf_include_domain" {
+  type = string
+}
+
+variable "dkim_record" {
+  type = object({
+    name  = string
+    value = string 
+  })
+}
+
+variable "record_exchanges" {
+  type = list(object({
+    preference = number
+    exchange = string
+  }))
+}
