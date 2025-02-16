fa66e837fc11b968f82683d9fd2ddf66e438d685
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-15 17:00:33 -0700

    updates formetadata in aca dapr tf

--- modules/container-app/aca-dapr-components.tf
+++ modules/container-app/aca-dapr-components.tf
@@ -16,8 +16,8 @@ resource "azurerm_container_app_environment_dapr_component" "aca_dapr_components
     for_each = each.value.metadata
     
     content {
-      name = metadata.name
-      value = metadata.name == "azureClientId" ? azurerm_user_assigned_identity.aca_user_identity.principal_id : metadata.value
+      name = metadata.value["name"]
+      value = metadata.value["name"] == "azureClientId" ? azurerm_user_assigned_identity.aca_user_identity.principal_id : metadata.value["value"]
     }
   }
 
