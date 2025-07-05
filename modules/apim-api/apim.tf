c1209ac48271f28d39d44691fca69ddd868a4702
Ronaldo Vecchi <rvecchi@gmail.com>
2025-07-05 05:51:57 -0600

    updates

--- modules/apim-api/apim.tf
+++ modules/apim-api/apim.tf
@@ -8,8 +8,8 @@ resource "azurerm_key_vault_access_policy" "apim_keyvault_policy" {
 }
 
 resource "azurerm_api_management_logger" "apim_appinsights_logger" {
-  name                = "${var.environment}-${var.domain}-appinsights-logger"
-  description         = "${title(var.environment)} ${var.domain} app insights logger for the api management instance"
+  name                = "${var.environment}-${var.domain}-${var.name_discriminator}-appinsights-logger"
+  description         = "${title(var.environment)} ${var.domain} ${var.name_discriminator} app insights logger for the api management instance"
   resource_group_name = var.apim_instance.resource_group_name
   api_management_name = var.apim_instance.name
   resource_id         = var.application_insights.id
--- modules/apim-simple-api/apim.tf
+++ modules/apim-simple-api/apim.tf
@@ -1,6 +1,6 @@
 resource "azurerm_api_management_logger" "apim_appinsights_logger" {
   name                = "${var.environment}-${var.domain}-${var.name_discriminator}-appinsights-logger"
-  description         = "${title(var.environment)} ${var.domain} app insights logger for the api management instance"
+  description         = "${title(var.environment)} ${var.domain} ${var.name_discriminator} app insights logger for the api management instance"
   resource_group_name = var.apim_instance.resource_group_name
   api_management_name = var.apim_instance.name
   resource_id         = var.application_insights.id
