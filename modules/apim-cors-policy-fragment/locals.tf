3cdc7fc93077e91a089c393cbcac55c5fbcdd193
Ronaldo Vecchi <rvecchi@gmail.com>
2025-07-05 16:41:38 -0600

    updates

--- modules/apim-cors-policy-fragment/locals.tf
+++ modules/apim-cors-policy-fragment/locals.tf
@@ -1,8 +1,3 @@
 locals {
-  tags = {
-    Environment = var.environment
-    Application = var.domain
-  }
-
   apim_allowed_origins = formatlist("<origin>%s</origin>", var.allowed_origins)
 }
--- modules/apim-jwtvalidate-policy-fragment/locals.tf
+++ modules/apim-jwtvalidate-policy-fragment/locals.tf
@@ -1,9 +1,4 @@
 locals {
-  tags = {
-    Environment = var.environment
-    Application = var.domain
-  }
-
   apim_allowed_audiences = formatlist("<audience>%s</audience>", var.audiences)
 
   apim_allowed_issuers = formatlist("<issuer>%s</issuer>", var.issuers)
